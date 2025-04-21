7bdd21fcc0d54d1bf1d7fa49dde3da33170b9abc
rafaelrvs <rafaelvieira1333@gmail.com>
2025-04-21 20:40:31 -0300

    correção layout

---
+++ src/app/head.js
@@ -0,0 +1,16 @@
+
+import { Script } from 'next/script';
+
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <script
+        data-ad-client="ca-pub-9552697386011881"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        crossOrigin="anonymous"
+      />
+    </>
+  );
+}
